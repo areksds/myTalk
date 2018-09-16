c2d6bd0c0e7bb36296064caa01431fc3727e6498
buildblox <info@buildblox.net>
2018-09-15 19:42:37 -0700

    Last time I promise

--- debate/lib/installer/upgrade.sql
+++ debate/lib/installer/upgrade.sql
@@ -1,1 +1,4 @@
-ALTER TABLE `events` ADD `chapter` INT(11) NULL DEFAULT NULL AFTER `region`;
+IF COL_LENGTH('events', 'chapter') IS NULL
+BEGIN
+	ALTER TABLE `events` ADD `chapter` INT(11) NULL DEFAULT NULL AFTER `region`;
+END
